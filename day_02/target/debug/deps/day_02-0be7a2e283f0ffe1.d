40c726b080f1f783356efceb266697803776beae
Scott O'Malley <scottyomalley@gmail.com>
2023-12-02 18:13:26 +0000

    Remove target folders

--- day_02/target/debug/deps/day_02-0be7a2e283f0ffe1.d
+++
@@ -1,6 +0,0 @@
-/Users/scottomalley/Projects/rust/AdventOfCode2023/day_02/target/debug/deps/libday_02-0be7a2e283f0ffe1.rmeta: src/main.rs src/../input.txt
-
-/Users/scottomalley/Projects/rust/AdventOfCode2023/day_02/target/debug/deps/day_02-0be7a2e283f0ffe1.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
